2a1d3a78843dbab784e22c7cbe473ef6afe4a534
Wendell Oliveira <wendellsilvaoliveira9@gmail.com>
2023-09-25 21:40:28 -0300

    Criação

---
+++ src/application/App.java
@@ -0,0 +1,46 @@
+package application;
+
+import entites.Carro;
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) {
+        int respostaUsuario;
+
+        do {
+            Carro carro = new Carro();
+                String marca = JOptionPane.showInputDialog(null,
+                        "Digite o nome da marca do Veiculo: ",
+                        "MARCA",
+                        JOptionPane.PLAIN_MESSAGE);
+                carro.setMarca(marca);
+
+            do {
+                String modelo = JOptionPane.showInputDialog(null,
+                        "Digite o modelo do Carro: ",
+                        "MODELO",
+                        JOptionPane.PLAIN_MESSAGE);
+                carro.setModelo(modelo);
+            } while (carro.retorno == false);
+
+            do {
+                String ano = JOptionPane.showInputDialog(null,
+                        "Digite o ano de Fabricação: ",
+                        "FABRICAÇÃO",
+                        JOptionPane.PLAIN_MESSAGE);
+                Integer anoFabricao = Integer.parseInt(ano);
+                carro.setAnoFabricacao(anoFabricao);
+            } while (carro.retorno == false);
+
+            JOptionPane.showMessageDialog(null,
+                    "Dados do Veiculo: \n"+carro.mostrarDados(),
+                    "DADOS?",
+                    JOptionPane.PLAIN_MESSAGE);
+
+            respostaUsuario = JOptionPane.showConfirmDialog(null,
+                    "Deseja Repetir?",
+                    "REPETIR?",
+                    JOptionPane.YES_NO_OPTION);
+        } while (respostaUsuario == JOptionPane.YES_OPTION);
+    }
+}
---
+++ src/entites/Carro.java
@@ -0,0 +1,70 @@
+package entites;
+
+import javax.swing.*;
+
+public class Carro {
+    private String marca;
+    private String modelo;
+    private int anoFabricacao;
+    public boolean retorno;
+    public boolean valModelo;
+
+    public boolean isValidarModelo() {
+        return this.valModelo;
+    }
+
+    public Carro() {
+    }
+
+    public Carro(String marca, String modelo, int anoFabricacao) {
+        this.setMarca(marca);
+        this.setModelo(modelo);
+        this.setAnoFabricacao(anoFabricacao);
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+            this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.valModelo = modelo.matches("[a-zA-Z]+");
+        if (valModelo == false) {
+            JOptionPane.showMessageDialog(null,
+                    "Digite um modelo sem CARCTERES ESPECIAIS!!",
+                    "ERRO!!",
+                    JOptionPane.ERROR_MESSAGE);
+            retorno = false;
+        } else {
+            this.modelo = modelo;
+            retorno = true;
+        }
+    }
+
+    public int getAnoFabricacao() {
+        return anoFabricacao;
+    }
+    public void setAnoFabricacao(int anoFabricacao) {
+        if (anoFabricacao < 0000) {
+                JOptionPane.showMessageDialog(null,
+                        "Digite uma idade válida",
+                        "IDADE INVáLIDA",
+                        JOptionPane.ERROR_MESSAGE);
+                retorno = false;
+        } else {
+            this.anoFabricacao = 2023 - anoFabricacao;
+            retorno = true;
+        }
+    }
+
+    public String mostrarDados() {
+        return "Marca: "+marca+
+                "\nModelo: "+modelo+
+                "\nAno de Fabricação: "+anoFabricacao;
+    }
+}
